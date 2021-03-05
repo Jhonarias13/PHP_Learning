ca225202b127d5faa34b38cac8b998be393ad774
Jhon Freiman Arias <jhonarias1322@gmail.com>
2021-03-04 20:42:30 -0500

    termiando con PHP

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
 
+$carrito = ['Tablet', 'Television', 'Computadora'];
+
+// util paraw ver los contenidos de un array
+echo "<hr>";
 
 
 
+// genera una linea en el DOM
+
+echo '<hr>';
+// acceder a los elementos de un array
+echo $carrito[1];
+
+
+//anade un indice en la posicion 3 del arreglo
+$carrito[3] = "nuevo producto..";
+
+
+//a;ade al final del arreglo
+array_push($carrito, 'audifonos');
+
+//a;ade al comienzo 
+array_unshift($carrito, 'dvd');
+
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
 include 'includes/footer.php';
